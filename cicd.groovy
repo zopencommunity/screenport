e79fbb8dd52f85b8d1d9dacfb5755c20b0d86590
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-05-23 12:43:30 -0400

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,6 @@
 node('linux')
 {
+  
   stage ('Poll') {
                 checkout([
                         $class: 'GitSCM',
@@ -8,6 +9,7 @@ node('linux')
                         extensions: [],
                         userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/screenport.git']]])
   }
+  
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/screenport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU Screen' )]
   }
